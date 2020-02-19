96aa46148e39314276cdb6795317e92290262e74
neddstarkk <nedheesh.hasija@gmail.com>
2020-02-19 13:41:43 +0530

    Welcome screen gradient included

---
+++ skybeat/lib/screens/music_home.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MusicHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- skybeat/lib/screens/welcome_screen.dart
+++ skybeat/lib/screens/welcome_screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:simple_animations/simple_animations.dart';
+import 'package:skybeat/screens/music_home.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tween = MultiTrackTween([
       Track("color1").add(Duration(seconds: 3),
-          ColorTween(begin: Color(0xFF0A0D33), end: Color(0xFF822B7B))),
+          ColorTween(begin: Color(0xFF0A0D33), end: Color(0xFFDF0F90))),
       Track("color2").add(Duration(seconds: 3),
-          ColorTween(begin: Color(0xFF822B7B), end: Color(0xFF0A0D33))),
+          ColorTween(begin: Color(0xFFDF0F90), end: Colors.blue.shade500)),
+      Track("color3").add(Duration(seconds: 3),
+          ColorTween(begin: Colors.blue.shade500, end: Color(0xFF0A0D33))),
     ]);
 
     return Scaffold(
@@ -22,7 +25,9 @@ class WelcomeScreen extends StatelessWidget {
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [
-                  animation["color1"], animation["color2"]
+                  animation["color1"],
+                  animation["color2"],
+                  animation["color3"]
                 ],
                 begin: Alignment.bottomLeft,
                 end: Alignment.topRight,
@@ -47,67 +52,87 @@ class WelcomeScreen extends StatelessWidget {
                   ),
                   SizedBox(
                     height: 70.0,
-                    width: MediaQuery
-                        .of(context)
-                        .size
-                        .width,
+                    width: MediaQuery.of(context).size.width,
                   ),
                   Column(
                     children: <Widget>[
-                      FlatButton(
-                        padding:
-                        EdgeInsets.symmetric(horizontal: 150.0, vertical: 15.0),
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: <Widget>[
-                            Icon(FontAwesomeIcons.facebookF),
-                            Text(
-                              '  Sign in with facebook',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                      ),
-                      SizedBox(
-                        height: 8.0,
-                      ),
-                      FlatButton(
-                        padding:
-                        EdgeInsets.symmetric(horizontal: 150.0, vertical: 15.0),
-                        color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        onPressed: () {},
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              FontAwesomeIcons.google,
-                              color: Colors.black,
-                            ),
-                            Text(
-                              '  Sign in with Google',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
+                      Container(
+                        height: 45.0,
+                        width: 425.0,
+                        child: FlatButton(
+                          color: Color(0xFF1553C6),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MusicHome(),
                               ),
-                            ),
-                          ],
+                            );
+                          },
+                          splashColor: Colors.grey.shade400,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(FontAwesomeIcons.facebookF),
+                              Text(
+                                'Sign in with facebook',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                       SizedBox(
-                        height: 25.0,
+                        height: 12.0,
+                        width: 5.0,
+                      ),
+                      Container(
+                        height: 45.0,
+                        width: 425.0,
+                        child: FlatButton(
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MusicHome(),
+                              ),
+                            );
+                          },
+                          splashColor: Colors.grey.shade400,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                FontAwesomeIcons.google,
+                                color: Colors.black,
+                              ),
+                              Text(
+                                '  Sign in with Google',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
                       ),
                       Text(
                         'By clicking log in, you agree to our Privacy Policy and Terms of Service',
-                        style: TextStyle(color: Colors.grey, fontSize: 10.0),
+                        style: TextStyle(color: Colors.grey, fontSize: 12.0),
                       ),
                       SizedBox(
-                        height: 10.0,
+                        height: 20.0,
                       )
                     ],
                   ),
@@ -116,8 +141,7 @@ class WelcomeScreen extends StatelessWidget {
             ),
           );
         },
-      )
-      ,
+      ),
     );
   }
 }

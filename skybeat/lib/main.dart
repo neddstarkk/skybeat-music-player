9aff5e82a0b53fa46ccaca50e46c895a36366938
neddstarkk <nedheesh.hasija@gmail.com>
2020-03-17 17:43:20 +0530

    Refactored more changes in splash screen and welcome screen

--- skybeat/lib/main.dart
+++ skybeat/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:skybeat/screens/splash_screen.dart';
+import 'package:skybeat/screens/splash_screen/splash_screen.dart';
 
 void main() => runApp(App());
 

729319337372a048101bbe0385b3ae7c9ebefaf1
neddstarkk <nedheesh.hasija@gmail.com>
2020-03-26 13:36:35 +0530

    UI: GenreSelector made by implementing ListView.

--- skybeat/lib/screens/explore_page/widgets/genre_selector.dart
+++ skybeat/lib/screens/explore_page/widgets/genre_selector.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:skybeat/screens/explore_page/widgets/quarter_circle_painter.dart';
 
+class Genre {
+  final String genreName;
+  final Color color;
+  final String image;
+
+  Genre({this.color, this.genreName, this.image});
+}
 
 class GenreSelector extends StatelessWidget {
-  final CircleAlignment circleAlignment;
-  final Color color;
-
-  GenreSelector(
-      {this.circleAlignment = CircleAlignment.bottomRight,
-      this.color = Colors.green});
+  final List<Genre> _genres = [
+    Genre(
+      color: Color(0xFFC74668),
+      genreName: "Romance",
+      image:
+          "https://www.ecopetit.cat/wpic/mpic/315-3155081_aesthetic-rose.jpg",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,21 +32,53 @@ class GenreSelector extends StatelessWidget {
             height: 150.0,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 2,
+              itemCount: 1,
               itemBuilder: (context, index) => Container(
                 width: 150.0,
                 margin: EdgeInsets.symmetric(horizontal: 8.0),
                 decoration: BoxDecoration(
-                  color: Colors.red,
+                  color: _genres[index].color,
                   borderRadius: BorderRadius.circular(20.0),
                 ),
-                child: ClipRect(
-                  child: CustomPaint(
-                    painter: QuarterCirclePainter(
-                      circleAlignment: circleAlignment,
-                      color: color,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 15.0,
                     ),
-                  ),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(
+                          width: 20.0,
+                        ),
+                        Text("${_genres[index].genreName}"),
+                      ],
+                    ),
+                    Spacer(),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Container(
+                        height: 85.0,
+                        width: 80.0,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(150.0),
+                              bottomRight: Radius.circular(15.0)),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(150.0),
+                            bottomRight: Radius.circular(15.0)
+                          ),
+                          child: Image.network(
+                            _genres[index].image,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
@@ -48,4 +88,3 @@ class GenreSelector extends StatelessWidget {
     );
   }
 }
-

183428db77e89c7ded5959bed88df26dfda18631
neddstarkk <nedheesh.hasija@gmail.com>
2020-03-27 17:09:58 +0530

    UI: Made the playlist gridview

--- skybeat/lib/screens/explore_page/explore_page.dart
+++ skybeat/lib/screens/explore_page/explore_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:simple_animations/simple_animations/multi_track_tween.dart';
 import 'package:skybeat/screens/explore_page/widgets/genre_selector.dart';
+import 'package:skybeat/screens/explore_page/widgets/playlists.dart';
 import 'package:skybeat/screens/explore_page/widgets/searchbar.dart';
 import 'package:skybeat/screens/explore_page/widgets/skybeat_charts.dart';
 
@@ -39,39 +40,64 @@ class ExplorePage extends StatelessWidget {
                       ),
                     ),
                   )),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).padding.top + 30),
-              SearchBar(),
-              SizedBox(
-                height: 25.0,
-              ),
-              GenreSelector(),
-              SizedBox(
-                height: 40.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    width: 20.0,
-                  ),
-                  Text(
-                    "Skybeat Charts",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 25.0,
-                      color: Color(0xFFCDCED3),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: MediaQuery.of(context).padding.top + 30),
+                SearchBar(),
+                SizedBox(
+                  height: 25.0,
+                ),
+                GenreSelector(),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20.0,
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              SkybeatCharts(),
-            ],
+                    Text(
+                      "Skybeat Charts",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        color: Color(0xFFCDCED3),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                SkybeatCharts(),
+                SizedBox(
+                  height: 40.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    Text(
+                      "Playlists For You",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25.0,
+                        color: Color(0xFFCDCED3),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25.0,
+                ),
+                Playlists(),
+              ],
+            ),
           ),
         ],
       ),
---
+++ skybeat/lib/screens/explore_page/widgets/playlists.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Playlist {
+  final String playlistName;
+  final String genre;
+  final String image;
+
+  Playlist({this.image, this.genre, this.playlistName});
+}
+
+class Playlists extends StatelessWidget {
+  final List<Playlist> _playlists = [
+    Playlist(
+        playlistName: "Punjabi Madhouse",
+        genre: "Pop \u00b7 Party",
+        image:
+            "https://i.pinimg.com/originals/85/0b/dc/850bdcfc75c370422f72a65d2fc5ddfc.jpg"),
+    Playlist(
+        playlistName: "Strictly Deep House ",
+        genre: "Excited \u00b7 Dance \u00b7 Party",
+        image:
+            "https://besthqwallpapers.com/img/original/119707/dj-console-dj-work-concepts-dance-party-dj-view-from-the-height.jpg"),
+    Playlist(
+        playlistName: "Drive Time Tunes",
+        genre: "Drive \u00b7 Karaoke \u00b7 Calm",
+        image: "https://assets3.thrillist.com/v1/image/2674681/size/tmg-facebook_social.jpg"),
+    Playlist(
+        playlistName: "No Pain No Gain",
+        genre: "Excited \u00b7 Workout \u00b7 Motivation",
+        image: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 0,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: GridView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: _playlists.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) => Column(
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  width: 200,
+                  margin: EdgeInsets.only(left: 10.0,),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(20.0)
+                  ),
+                  child: Image.network("${_playlists[index].image}", fit: BoxFit.fill,),
+                ),
+                Text("${_playlists[index].playlistName}"),
+                Text("${_playlists[index].genre}"),
+              ],
+            )),
+      ),
+    );
+  }
+}

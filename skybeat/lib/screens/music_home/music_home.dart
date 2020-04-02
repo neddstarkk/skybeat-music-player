e7e3c52c7c495765747177f63f30e1b0a9589544
neddstarkk <nedheesh.hasija@gmail.com>
2020-04-02 09:44:23 +0530

    UI: Made the appbar on Music Screen

--- skybeat/lib/screens/music_home/music_home.dart
+++ skybeat/lib/screens/music_home/music_home.dart
@@ -1,15 +1,38 @@
 import 'package:flutter/material.dart';
 
-class MusicHome extends StatelessWidget {
-
+class MusicHome extends StatefulWidget {
   const MusicHome({Key key}) : super(key: key);
 
+  @override
+  _MusicHomeState createState() => _MusicHomeState();
+}
 
+class _MusicHomeState extends State<MusicHome> {
+
+  
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Center(
-      child: Text("Music Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent.withOpacity(0.0),
+        elevation: 0,
+        title: Text(
+          "Your Daily Mix",
+          style: TextStyle(color: Color(0xFFCDCED3), fontSize: 15.0),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          Icon(
+            Icons.more_horiz,
+          ),
+          SizedBox(
+            width: 10.0,
+          )
+        ],
+        leading: Icon(Icons.sort),
+      ),
+      body: Stack(),
     );
   }
 }
